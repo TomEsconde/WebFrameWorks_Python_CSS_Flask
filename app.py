4d3b16ac4ba202787b262b79063b24863a846551
Tom Esconde <Tom_Esconde@hotmail.com>
2023-02-19 13:43:47 +0100

    withdrawal and deposit done

--- app.py
+++ app.py
@@ -1,35 +1,214 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, upgrade
 
 from model import db, seedData, Customer
+from forms import NewCustomerForm
 
+from flask_security import roles_accepted, auth_required, logout_user
+from model import Customer, Account, Transaction
+from model import db, seedData
+from forms import NewCustomerForm, Depositform, Withdrawform
+from datetime import datetime
+import os
 
  
+# app = Flask(__name__)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Kalleballe87@localhost/bank'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT", '146585145368132386173505678016728509634')
+app.config["REMEMBER_COOKIE_SAMESITE"] = "strict"
+app.config["SESSION_COOKIE_SAMESITE"] = "strict"
+# app.config['SECRET_KEY'] = os.urandom(32)
 db.app = app
 db.init_app(app)
 migrate = Migrate(app,db)
  
  
+ 
 
 @app.route("/")
 def startpage():
-    return "hej"
-    #trendingCategories = Category.query.all()
-    #return render_template("index.html", trendingCategories=trendingCategories)
+    # trendingCategories = Customer.query.all()
+    account = Account.query.filter(Account.Balance)
+    balance = 0
+    allAccount = Account.query.count()
+    customers = Customer.query.count()
+    for x in account:
+        balance += x.Balance
+    return render_template("index.html", balance=balance, allAccount = allAccount, customers = customers, redirect="/")
 
 @app.route("/customer/<id>")
-def customers():
-    customers = Customer.query.filter_by(Id =id).first()
-    return render_template("customer.html", customer=customers)
+@auth_required()
+@roles_accepted("Admin", "Staff")
+def customer(id):
+    customer = Customer.query.filter_by(Id =id).first()
+    account = Account.query.filter_by(Id = id).first()
+    Saldo = 0
+    for accounts in customer.Accounts:
+        Saldo = Saldo + accounts.Balance
+    return render_template("customer.html", customer=customer, account = account, Saldo=Saldo)
+
+
+
+# @app.route("/transfer/<id>", methods=['GET', 'POST'])
+# @auth_required()
+# @roles_accepted("Admin")
+# def withdraw(id):
+#     form = Transferform()
+#     account = Account.query.filter_by(Id = id).first()
+#     customer = Customer.query.filter_by(Id =id).first()
+#     date = datetime.now()
+#     large = ["too large"]
+#     if form.validate_on_submit():
+#         if account.Balance < form.Amount.data:
+#             form.Amount.errors = form.Amount.errors + large
+            
+#         else:
+#             account.Balance = account.Balance - form.Amount.data
+#             t = Transaction()
+#             t.AccountId = account.Id
+#             t.Operation = "Bank withdrawal"
+
+#             t.Type = "Credit"
+#             t.Amount = form.Amount.data
+#             t.NewBalance = account.Balance
+#             t.Date = datetime.now()
+#             db.session.add(t)
+#             db.session.commit()
+#             return redirect("/customer/" + str(account.CustomerId) )
+#     return render_template("transfer.html", form=form, account= account, customer = customer, date=date )
+
+
+############## Transfer slutar här
+
+
+
+@app.route("/deposit/<id>", methods=['GET', 'POST'])
+@auth_required()
+@roles_accepted("Admin")
+def deposit(id):
+    form = Depositform()
+    account = Account.query.filter_by(Id = id).first()
+    customer = Customer.query.filter_by(Id =id).first()
+    t = Transaction()
+    date = datetime.now()
+    if form.validate_on_submit():
+        account.Balance = account.Balance + form.Amount.data
+        # db.session.commit()
+        t.AccountId = account.Id
+        t.Operation = "Transfer"
+
+        t.Type = "Credit"
+        t.Amount = form.Amount.data
+        t.NewBalance = account.Balance
+        t.Date = datetime.now()
+        db.session.add(t)
+        db.session.commit()
+        return redirect("/customer/" + str(account.CustomerId) )
+    return render_template("deposit.html", form=form, account= account, customer = customer, date=date )
+    
+@app.route("/withdraw/<id>", methods=['GET', 'POST'])
+@auth_required()
+@roles_accepted("Admin")
+def withdraw(id):
+    form = Withdrawform()
+    account = Account.query.filter_by(Id = id).first()
+    customer = Customer.query.filter_by(Id =id).first()
+    date = datetime.now()
+
+    # ownValidationOk = True
+    # if request.method == 'POST':
+    #     form.Amount.errors = form.Amount.errors + ('too large',)
+    #     ownValidationOk = False
+    
+    
+    large = ["too large"]
+    if form.validate_on_submit():
+        if account.Balance < form.Amount.data:
+            form.Amount.errors = form.Amount.errors + large
+            
+        else:
+            account.Balance = account.Balance - form.Amount.data
+            t = Transaction()
+            t.AccountId = account.Id
+            t.Operation = "Bank withdrawal"
+
+            t.Type = "Credit"
+            t.Amount = form.Amount.data
+            t.NewBalance = account.Balance
+            t.Date = datetime.now()
+            db.session.add(t)
+            db.session.commit()
+            return redirect("/customer/" + str(account.CustomerId) )
+    return render_template("withdraw.html", form=form, account= account, customer = customer, date=date )
+
+@app.route("/notEnoughBalance/<id>", methods=['GET', 'POST'])
+@auth_required()
+@roles_accepted("Admin")
+def notEnoughBalance(id):
+    account = Account.query.filter_by(Id = id).first()
+    customer = Customer.query.filter_by(Id =id).first()
+    return render_template("notEnoughBalance.html", account=account, customer=customer)
     
 
+
+
+@app.route("/transactions/<id>")
+# @auth_required()
+# @roles_accepted("Admin", "Staff")
+def transactions(id):
+    accountid = Account.query.filter_by(Id =id).first()
+    transactions = Transaction.query.filter_by(AccountId=id)
+    return render_template("transactions.html", accountid = accountid, transactions=transactions)
+
+
+@app.route("/admin")
+@auth_required()
+@roles_accepted("Admin")
+def adminpage():
+    return render_template("admin.html", activePage="secretPage" )
+
+
 @app.route("/customers")
-def customer():
-    customers = Customer.query.all()
-    return render_template("customers.html", customers=customers)
+@auth_required()
+@roles_accepted("Admin", "Staff")
+def customers():
+    sortColumn = request.args.get('sortColumn', 'namn')
+    sortOrder = request.args.get('sortOrder', 'asc')
+    q = request.args.get('q', '')
+    page = int(request.args.get('page', 1))
+    
+
+    customers = Customer.query
+
+    customers = customers.filter(
+        Customer.GivenName.like('%' + q + '%') |
+        Customer.City.like('%' + q + '%'))
+
+    if sortColumn == "namn":
+        if sortOrder =="asc":
+            customers = customers.order_by(Customer.Surname.asc())
+        else:
+            customers = customers.order_by(Customer.Surname.desc())
+    elif sortColumn == "city":
+        if sortOrder == "asc":
+            customers = customers.order_by(Customer.City.asc())
+        else:
+            customers = customers.order_by(Customer.City.desc())
+
+    paginationObject = customers.paginate(page=page, per_page=50, error_out=False)
+
+    return render_template("customers.html", 
+    customers=paginationObject.items,
+    pages=paginationObject.pages,
+    sortOrder=sortOrder,
+    sortColumn=sortColumn,
+    has_next=paginationObject.has_next,
+    has_prev=paginationObject.has_prev,
+    page=page, 
+    q=q)
     # return render_template("category.html", products=products)
 
 @app.route("/category/<id>")
@@ -38,11 +217,42 @@ def category(id):
     # products = Product.query.all()
     # return render_template("category.html", products=products)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect("/")
+
+@app.route("/newcustomer", methods=['GET', 'POST'])
+@auth_required()
+@roles_accepted("Admin", "Staff")
+def newcustomer():
+    form = NewCustomerForm()
+    if form.validate_on_submit():
+        #spara i databas
+        customer = Customer()
+        customer.GivenName = form.GivenName.data
+        customer.Surname = form.Surname.data
+        customer.Streetaddress = form.Streetaddress.data
+        customer.City = form.City.data
+        customer.Zipcode = form.Zipcode.data
+        customer.Country = form.Country.data
+        customer.CountryCode = form.CountryCode.data
+        customer.Birthday = form.Birthday.data
+        customer.NationalId = form.NationalId.data
+        customer.TelephoneCountryCode = form.TelephoneCountryCode.data
+        customer.Telephone = form.Telephone.data
+        customer.EmailAddress = form.EmailAddress.data
+
+        db.session.add(customer)
+        db.session.commit()
+        return redirect("/customers" )
+    return render_template("newcustomer.html", formen=form )
+
 
 if __name__  == "__main__":
     with app.app_context():
         upgrade()
     
-        seedData(db)
-        app.run()
+        seedData(app, db)
+        app.run(debug = True)
 
---
+++ forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, ValidationError
+from wtforms.fields import IntegerField, SelectField, DateField, DecimalField
+
+def emailContains(form, field):
+    if not field.data.endswith('.se'):
+        raise ValidationError('Måste sluta på .se dummer')
+
+
+class NewCustomerForm(FlaskForm):
+    GivenName = StringField('GivenName', validators=[validators.DataRequired()])
+    Surname = StringField('Surname', validators=[validators.DataRequired()])
+    Streetaddress = StringField('Streetaddress', validators=[validators.DataRequired()])
+    City = StringField('City', validators=[validators.DataRequired()])
+    Zipcode = IntegerField('Zipcode', validators=[validators.DataRequired()])
+    Country = StringField('Country', validators=[validators.DataRequired()])
+    CountryCode = StringField('CountryCode', validators=[validators.DataRequired()])
+    Birthday= DateField('Birthday', validators=[validators.DataRequired()])
+    NationalId = IntegerField('NationalId', validators=[validators.DataRequired()])
+    TelephoneCountryCode = SelectField('TelephoneCountryCode',choices=[('46','+46'),('41','+41'),('42','+42')])
+    Telephone = IntegerField('Telephone', validators=[validators.DataRequired()])
+    EmailAddress = StringField('EmailAddress', validators=[validators.DataRequired(), emailContains])
+
+class Depositform(FlaskForm):
+    Amount = IntegerField('Amount', validators=[validators.DataRequired(), validators.NumberRange(min=1,max=1000000)])
+
+class Withdrawform(FlaskForm):
+    Amount = IntegerField('Amount', validators=[validators.DataRequired(), validators.NumberRange(min=1,max=1000000)])
+
+class Transferform(FlaskForm):
+    Amount = IntegerField('Amount', validators=[validators.DataRequired(), validators.NumberRange(min=1,max=1000000)])
